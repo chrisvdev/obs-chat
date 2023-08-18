d67cbbf945307b7d69c27db675dae4bc54ee2cb4
Christian Villegas <chrisvill.2312@gmail.com>
2023-08-18 20:42:05 -0300

    FIX: deleted Dist

--- dist/assets/load_css.js
+++
@@ -1,9 +0,0 @@
-import getURLParams from "./get_url_params.js"
-
-const params = getURLParams()
-if (params.style) {
-    const externalStyle = document.createElement("link")
-    externalStyle.rel = "stylesheet"
-    externalStyle.href = params.style
-    document.head.appendChild(externalStyle)
-}
