3334f2c94b959c809bc2d4e605fad289b03648c2
Christian Villegas <chrisvill.2312@gmail.com>
2023-10-16 20:56:33 -0300

    🐛 FIX: Style loading

--- public/assets/load_css.js
+++ public/assets/load_css.js
@@ -1,5 +1,7 @@
 import getURLParams from './get_url_params.js'
 
+const { style } = JSON.parse(localStorage.getItem('variables')) || {}
+
 const params = getURLParams()
 if (params.style) {
   fetch(params.style)
@@ -9,4 +11,13 @@ if (params.style) {
       externalStyle.innerText = response
       document.body.appendChild(externalStyle)
     })
+} else {
+  if (style)
+    fetch(style)
+      .then((response) => response.text())
+      .then((response) => {
+        const externalStyle = document.createElement('style')
+        externalStyle.innerText = response
+        document.body.appendChild(externalStyle)
+      })
 }
