988030c3b838f046565b223fc0a3f140d3dbc2b7
Christian Villegas <chrisvill.2312@gmail.com>
2024-03-25 21:35:17 -0300

    ✨ FEAT: mentions

--- src/middlewares/to_tts.js
+++ src/middlewares/to_tts.js
@@ -2,19 +2,25 @@ import tts from '../lib/tts'
 import getVariable, {
   TTS_ALWAYS_ON,
   TTS_ACCENT,
-  TTS_INDEX
+  TTS_INDEX,
+  TTS_MENTIONS
 } from '../lib/get_variable'
 
 const ttsAlwaysOn = getVariable(TTS_ALWAYS_ON)
 const ttsAccent = getVariable(TTS_ACCENT)
 const ttsIndex = getVariable(TTS_INDEX)
+const ttsMentions = getVariable(TTS_MENTIONS)
+
+let lastUserName = null
 
 export default function toTTS(message) {
   const { speak, userName } = message
   if (speak) {
     const { toRead, accent, variant } = speak
-    if (ttsAlwaysOn) tts.speak(`${userName} dicé`, ttsAccent, ttsIndex)
+    if (ttsAlwaysOn && ttsMentions && lastUserName !== userName)
+      tts.speak(`${userName} dicé`, ttsAccent, ttsIndex)
     tts.speak(toRead, accent, variant)
   }
+  lastUserName = userName
   return message
 }
